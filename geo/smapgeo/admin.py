17d7733422603c350ae80af3894a109de602556a
Gabe Fierro <gtfierro225@gmail.com>
2012-06-22 15:26:57 -0700

    changes to admin for it to work

--- geo/smapgeo/admin.py
+++ geo/smapgeo/admin.py
@@ -2,10 +2,10 @@ from smapgeo.models import Building, Floor, View, Area, AreaMetadata, NodeLink
 from django.contrib.gis import admin
 from olwidget.admin import GeoModelAdmin
 
-class OSMAdmin(admin.OSMGeoAdmin):
-	zoom_level = 8
-	default_lat = 37.871775
-	default_lon = -122.274603
+#class OSMAdmin(admin.OSMGeoAdmin):
+#	zoom_level = 8
+#	default_lat = 37.871775
+#	default_lon = -122.274603
 
 class GoogleMapsAdmin(GeoModelAdmin):
     options = {
