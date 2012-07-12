c77d375c2803db6657b15d5cefd920f796cc9cbc
Nikita Kitaev <nikitakit@gmail.com>
2012-07-12 16:06:57 -0700

    Extend jQuery plugin to support client-side query API access

--- web/media/jquery.appstack.js
+++ web/media/jquery.appstack.js
@@ -2,6 +2,8 @@
 
 (function($) { //Hide scope, no $ conflict
 
+  //////////////////////////////// Geo display via SVG ////////////////////////////////
+
   function AppstackManager() {
 	this.building = null;
 	this.floors = [];
@@ -90,17 +92,46 @@
 
 	_building_nameAppstack: function(container) {
 	  var $container = $(container)
-	  if ($container.is('g')) {
+	  if ($container.is('svg')){
+		// We have a building
+		return $container.children('title').text();
+	  } else if ($container.is('g')) {
 		// We have a floor
 		return $container.parent('svg').children('title').text();
 	  } else if ($container.is('path')) {
 		// We have an area
 		return $container.parent('g').parent('svg').children('title').text();
-	  } else if ($container.is('svg')){
-		return $container.children('title').text();
 	  } else {
 		return undefined;
 	  }
+	},
+
+	_queryAppstack: function(container) {
+	  var $container = $(container)
+	  if ($container.is('svg')){
+		// We have a building
+		return '!' + $container.appstack('building_name')
+	  } else if ($container.is('g')) {
+		// We have a floor
+		return '!' + $container.appstack('floor_name')  + '<!' + $container.appstack('building_name')
+	  } else if ($container.is('path')) {
+		// We have an area
+		return '!' + $container.appstack('area_name') + '<!' + $container.appstack('floor_name')  + '<!' + $container.appstack('building_name')
+	  } else {
+		return undefined;
+	  }
+
+	},
+
+	_devicesAppstack: function(container, callback) {
+	  var $container = $(container)
+	  var query = $container.appstack('query');
+	  if (query === undefined) {
+		return undefined;
+	  }
+	  query = '. < ' + query;
+	  $.q(query, callback);
+	  return $container;
 	},
 
   })
@@ -116,5 +147,121 @@
   }
 
   $.appstack = new AppstackManager();
+
+  //////////////////////////////// Queries ////////////////////////////////
+
+
+  var appQuery = function( query, fn ) {
+	if (typeof query === "object") {
+	  return new appQuery.fn.init( query )
+	} else if (typeof query === "string" && typeof fn === "function") {
+	  $.ajax(appQuery.settings.url + "/webapi/query?" + $.param({'q': query}),
+			 {
+			 }).done( function ( data ) {
+			   fn( new appQuery.fn.init( data ));
+			 });
+	  return undefined;
+	} else {
+	  throw "Invalid arguments";
+	}
+  }
+
+  appQuery.settings = {
+	url: "http://127.0.0.1:8000"
+  }
+
+  appQuery.fn = appQuery.prototype = {
+	constructor: appQuery,
+	init: function( objs ) {
+	  if ($.isArray(objs)) {
+		this.objects = objs;
+	  } else {
+		this.objects = [objs];
+	  }
+	  return this;
+	},
+
+	setup: function( settings ) {
+	  $.extend( appQuery.settings, settings );
+	},
+
+	each: function( fn ) {
+	  $.each(this.objects, function(i, dev){
+		fn(i, appQuery(dev));
+	  });
+	  return this;
+	},
+
+	filter: function( type ) {
+	  var keyword = type.slice(0, 1);
+	  var match = type.slice(1);
+	  if (keyword === '!') {
+		this.objects = this.objects.filter( function(obj) {
+		  if ( obj.type === 'Area' || obj.type === 'Floor' || obj.type === 'Building') {
+			if ( obj.name.search(match) !== -1 ) {
+			  return true;
+			}
+		  }
+		  return false;
+		});
+		return this;
+	  } else if (keyword === '.') {
+		keyword = 'type';
+	  } else if (keyword === '^') {
+		keyword = 'uuid';
+	  } else if (keyword === '$') {
+		keyword = 'name';
+	  } else {
+		this.objects = [];
+		return this;
+	  }
+	  this.objects = this.objects.filter( function(obj) { return obj[keyword] === match; });
+	  return this;
+	},
+
+	call: function( method ) {
+	  var param;
+	  var args = Array.prototype.slice.call(arguments, 1);
+	  var callback = function(obj, response) {};
+	  if (typeof args.slice(-1)[0] === "function") {
+		callback = args.slice(-1)[0];
+		args = args.slice(0, -1);
+	  }
+
+	  if (args.length === 1 && typeof args[0] === "object") {
+		param = $.param(args[0]);
+	  } else {
+		param = "";
+		for (var i = 0, j = args.length; i < j; i++) {
+		  param += "," + JSON.stringify(args[i]);
+		}
+		param = param.slice(1);
+	  }
+	  $.each(this.objects, function(i, obj) {
+		if (obj.hasOwnProperty("methods") && obj.methods.hasOwnProperty(method)) {
+		  var url = appQuery.settings.url + "/webapi/uuid/" + obj.uuid + "/" + method + "?" + param;
+		  $.ajax(url, {}).done( function( data ) {
+			callback(obj, data);
+		  });
+		}
+	  });
+	},
+
+	get: function( s ) {
+	  if (s === 'uuid') {
+		return this.objects[0].uuid;
+	  } else if (s === 'name') {
+		return this.objects[0].name;
+	  } else if (s === 'type') {
+		return this.objects[0].type;
+	  } else if (s === 'methods') {
+		return this.objects[0].methods;
+	  }
+	}
+  }
+
+  appQuery.fn.init.prototype = appQuery.prototype;
+
+  $.q = appQuery;
 
 })(jQuery);
