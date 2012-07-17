fb8f179638e8263a05725c14ffebb353a0c5d1c4
Gabe Fierro <gtfierro225@gmail.com>
2012-07-17 13:18:22 -0700

    working on autoformatting for bancroft

--- format/format_building.py
+++ format/format_building.py
@@ -1,50 +1,73 @@
+import sys
 import os
 import re
 import pickle
-import sys
-sys.path.append('../')
 import suds
+sys.path.append('../')
 import node_types
-#from generate_geo import *
+from generate_geo import *
 from collections import defaultdict
 from fuzzywuzzy import process
+from pprint import pprint
+
+url = "https://emsalc.berkeley.edu/_common/webservices/Eval?wsdl"
+client = suds.client.Client(url, username="akrioukov", password="Tangle57")
+
+def get_children(path):
+    global client
+    return client.service.getFilteredChildren(path, "WEB_GEO")
+
+def get_value(path):
+    global client
+    return client.service.getValue(path)
+
+alc_children = []
+
+def store_child(path):
+    children = get_children(path)
+    if len(children) == 0:
+      alc_children.append(path)
+      print path
+    for c in children:
+        c = c.referenceName
+        child_path = path + c if path == '/' else path + '/' + c
+        store_child(child_path)
+
+store_child('#doe_library')
 
 
 class DictGen(object):
-  """
-  generates the dictionary before we hand it off to the Formatter to actually generate the file.
+  """ 
+  generates the dictionary before we hand it off to the Formatter to actually
+  generate the file.
 
-  We will index the dictionary by relational name ('hvac','lights',etc), which contains a list of
-  object types ('VAV','AHU','LIG',etc), each of which contains a list of
-  object names ('vav 1','ahu 3', 'light bank 4'), each of which contains a dictionary of
-    location: 'doe_library','doe_4th_floor', etc
-    points: ('m304',discharge air temp),('asdf',high_temp_request), etc
+  We will index the dictionary by relational name ('hvac','lights',etc), which
+  contains a list of object types ('VAV','AHU','LIG',etc), each of which
+  contains a list of object names ('vav 1','ahu 3', 'light bank 4'), each of
+  which contains a dictionary of location: 'doe_library','doe_4th_floor', etc
+  points: ('m304',discharge air temp),('asdf',high_temp_request), etc.
   """
 
-  def __init__(self, picklefile, building_name):
+  def __init__(self, building_name):
     """
-    [picklefile] is a pickled file (string, we'll open() it) of all the ALC points.
     [building_name] is a string of the name of the building
     """
-    self.file = pickle.load(open(picklefile))
     self.building_name = building_name
     self.building_dict = defaultdict(lambda : defaultdict(lambda : defaultdict(dict)))
-    self.url = "https://emsalc.berkeley.edu/_common/webservices/Eval?wsdl"
-    self.client = suds.client.Client(self.url, username="akrioukov", password="Tangle57")
-    self.children_dict = {}
     #get dictionary of children so we only have to access SOAP the one time
     print('generating dictionary of points and children...')
-    for v in self.file.itervalues():
+    self.children_dict = {}
+    for v in alc_children:
       #fancy status bar because I was bored
-      sys.stdout.write('\r'+str(self.file.values().index(v) * 100.0  / float(len(self.file))) + '%')
+      sys.stdout.write('\r'+str((alc_children.index(v)+1) * 100.0  / float(len(alc_children))) + '%')
       sys.stdout.flush()
       path = '/'.join(v.split('/')[:-1])
-      self.children_dict[v] = self.client.service.getFilteredChildren(path,'WEB_GEO')
+      self.children_dict[v] = client.service.getFilteredChildren(path,'WEB_GEO')
 
   def make_building(self):
     """
-    uses generate_geo.py along with self.file to create the geodjango objects based
-    on the geo paths exposed by ALC
+    uses generate_geo.py along with alc_children to create the geodjango objects
+    based on the geo paths exposed by ALC
     """
     #define regex for recognizing components
     building = lambda x: re.search('(#doe_library)',x)
@@ -55,7 +78,7 @@ class DictGen(object):
     floors = []
     areas = []
 
-    for v in self.file.itervalues():
+    for v in alc_children:
       res = building(v)
       if res:
         bu = res.groups()[0]
@@ -66,13 +89,21 @@ class DictGen(object):
         fl = res.groups()[0]
         if fl not in floors:
           floors.append(fl)
-    print buildings, floors
+    print 'Buildings:',buildings
+    print 'Floors:',floors
+    for b in buildings:
+      b = Building(b, floors)
+      for hvaczone in self.building_dict['hvac']['VAV']:
+        print self.building_dict['hvac']['VAV'][hvaczone]['location'][-2]
+        b.add_area(self.building_dict['hvac']['VAV'][hvaczone]['location'][-2][5:],hvaczone,'hvac')
+      b.register()
 
   def generate_dict(self, keyword,type,relational):
     """
-    take keyword (search argument) and search the children_dict for strings containing it. Then
-    cast these to objects of type [type] and put it in the building_dict under relational [relational]
-    For example, use the keyword 'vav' with type 'VAV' to look for vavs.
+    take keyword (search argument) and search the children_dict for strings
+    containing it. Then cast these to objects of type [type] and put it in the
+    building_dict under relational [relational] For example, use the keyword
+    'vav' with type 'VAV' to look for vavs.
     Will return a dictionary like so:
     'VAV': {
         'vav 1': {
@@ -83,7 +114,7 @@ class DictGen(object):
           ...
     """
     d = defaultdict(lambda : defaultdict(list))
-    for v in self.file.itervalues():
+    for v in alc_children:
       if keyword in v:
         string = filter(lambda x: keyword in x, v.split('/'))[0]
         point = v.split('/')[-1]
@@ -103,7 +134,8 @@ class Formatter(object):
 
   def __init__(self, building):
     self.building = building
-    with open(self.building+'.py','wb') as f:
+    self.building_dict = pickle.load(open(building+'.db'))
+    with open('test.py','wb') as f:
       #write all the imports
       f.write("""from node import Relational
 from generic_objects import *
@@ -114,7 +146,6 @@ import node_types
 
 gis.NodeLink.objects.all().delete()\n""")
 
-
   def setlist(self, list_file):
     self.list_file = pickle.load(open(list_file))
 
@@ -123,7 +154,7 @@ gis.NodeLink.objects.all().delete()\n""")
     with open(self.building+'.py','a') as f:
       f.write("%s = relational('%s')\n" % (relational_type,relational_type))
 
-  def build(self):
+  def build_objects(self):
     f = open(self.building+'.py','a')
 
     for line in self.list_file:
@@ -140,3 +171,14 @@ gis.NodeLink.objects.all().delete()\n""")
       f.write(line_string)
 
     f.close()
+
+if __name__=='__main__':
+  d = DictGen('Bancroft Library')
+  d.generate_dict('vav','VAV','hvac')
+  d.generate_dict('ah', 'AHU','hvac')
+  d.make_building()
+  pickle.dump(d.building_dict,open('alc_bancroft.db','w'))
+  f = Formatter('Bancroft Library')
+
+
+
