80ab870a6e1aaf94d48715c6d0f398f53370ef87
Gabe Fierro <gtfierro225@gmail.com>
2013-02-22 13:52:49 -0800

    clean up old files

--- python/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='appstack',
-      version='0.1',
-      description='Building application stack API wrapper',
-      author='Nikita Kitaev',
-      author_email='nikitakit@gmail.com',
-      packages=['appstack'],
-      requires=['requests']
-      )
