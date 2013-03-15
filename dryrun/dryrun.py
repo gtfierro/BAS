4f73c8812ee8adc19f5509c006faa366663b07fc
Gabe Fierro <gtfierro225@gmail.com>
2013-03-15 11:16:19 -0700

    interpret POST data as dictionary

--- dryrun/dryrun.py
+++ dryrun/dryrun.py
@@ -4,6 +4,7 @@ from flask import jsonify
 import redis
 import time
 import random
+import ast
 
 rdb = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -15,9 +16,10 @@ def read_point(point):
         rdb.set(point, random.randint(0,3))
     return jsonify({'Readings': (time.time(),rdb.get(point)) })
 
-@app.route('/<path:point>/write', methods=['POST'])
+@app.route('/<path:point>', methods=['POST','PUT'])
 def write_point(point):
-    rdb.set(point, request.form['value'])
+    data = ast.literal_eval(request.data)
+    rdb.set(point, data['value'])
     return jsonify({point: rdb.get(point)})
  
 if __name__=="__main__":
