f6e415b7ed972a7f46975f0e428533f4553bb95c
Gabe Fierro <gtfierro225@gmail.com>
2013-03-17 11:23:20 -0700

    add openbms sMAP uids to sdh_demo

--- sdh_demo.py
+++ sdh_demo.py
@@ -10,74 +10,74 @@ import node_types
 l = Relational('Lights')
 # Floor 4
 lightbank4_1 = LIG(l, 'Light Bank 1', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY12"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY11")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY12",'6c87b20e-454a-5343-8776-f7377aa105f1'),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY11",'b48ca62b-425c-54a5-8b9e-87784201eef2')
                         })
 lightbank4_2 = LIG(l, 'Light Bank 2', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY10"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY09")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY10",'441afaab-1712-56a1-a55e-e405601edfc2'),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY09",'7bf7569c-4578-52ed-8a65-08e695206a08')
                         })
 lightbank4_3 = LIG(l, 'Light Bank 3', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY08"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY07")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY08",'6841a6f5-0820-5e43-becb-453bffb2c990'),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY07",'12684335-8922-548b-bc10-a896b2a4e713')
                         })
 lightbank4_4 = LIG(l, 'Light Bank 4', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY04"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY03")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY04","960db648-0d55-5cd8-abb7-f8243be52d7d"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY03","f4d8ce79-20e6-55e1-8dcc-7872dcf849a5")
                       })
 
 lightbank4_5 = LIG(l, 'Light Bank 5', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY06"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY05")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86007/RELAY06","c11a58b4-c806-5296-b542-ba093d5ed71e"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86007/RELAY05","b5dcb3f4-7083-5988-b4a1-119efc7623a5")
                         })
 # Floor 5
 lightbank5_1 = LIG(l, 'Light Bank 1', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY10"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY11")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY10","07f8b407-ab1e-5b9e-b0f9-706df33836c2"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY11","6d17ed21-1d71-5872-9bcd-11325dbde034")
                         })
 lightbank5_2 = LIG(l, 'Light Bank 2', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY12"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY13")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY12","caa7cd5a-1de7-5f19-aed2-7da21169c1cc"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY13","b957d4e1-52cb-5f82-8739-e07cc34fca20")
                         })
 lightbank5_3 = LIG(l, 'Light Bank 3', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY07"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY06")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY07","7d489e43-dedf-5964-9b07-58231250950c"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY06","8f74e013-1fd3-55d4-8fc3-f62cb3034ccf")
                         })
 lightbank5_4 = LIG(l, 'Light Bank 4', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY09"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY08")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY09","47cf2d95-e654-5012-8e0c-5a318a95bd72"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY08","73192153-d25b-502c-a46d-98b6dba06d32")
                         })
 lightbank5_5 = LIG(l, 'Light Bank 5', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY05"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY04")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY05","08680d31-28cb-5092-890a-a1541c027c10"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY04","92fce355-f665-5cf3-9f68-dda7454e4cc2")
                         })
 lightbank5_6 = LIG(l, 'Light Bank 6', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY03"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY02")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86008/RELAY03","b38284da-a9ed-5b40-a206-f3e4f3302f83"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86008/RELAY02","c528235b-d4d0-544f-86b3-2e24536cce0b")
                         })
 
 # Floor 6
 lightbank6_1 = LIG(l, 'Light Bank 1', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86010/RELAY03"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86010/RELAY02")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86010/RELAY03","2d9f2ffd-dd53-5e64-8ed6-eb57441f83b7"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86010/RELAY02","33ee9b3c-2891-579f-8106-137f5a281d6b")
                       })
 
 # Floor 7
 lightbank7_1 = LIG(l, 'Light Bank 1', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY09"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY08")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY09","070af478-b12b-53ea-b0b9-c7833870723b"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY08","80df2c64-36d2-58d5-bb80-16999eb94408")
                       })
 lightbank7_2 = LIG(l, 'Light Bank 2', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY11"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY10")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY11","0deb8b0e-5d9c-5e47-abb2-3b33b2570687"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY10","68fe6fd7-611a-50cf-a020-aebb5f222618")
                         })
 lightbank7_3 = LIG(l, 'Light Bank 3', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY05"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY04")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY05","bbb2b35a-4bba-504a-a904-ace590e86449"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY04","e25a1409-f2ac-585c-92d2-f712126d3321")
                         })
 lightbank7_4 = LIG(l, 'Light Bank 4', {
-                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY07"),
-                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY06")
+                        'LO_REL' : BACnetREL( "Low Relay", "/WS86011/RELAY07","9c3b8e2a-b5cc-58fe-b408-264ad767daf9"),
+                        'HI_REL' : BACnetREL( "High Relay", "/WS86011/RELAY06","2ceedbb3-571e-501a-a3b4-aaab6d79c30f")
                       })
 
 
