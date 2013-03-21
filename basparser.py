1af07ead33b217ff3c6e63bf992098b580bc41af
Gabe Fierro <gtfierro225@gmail.com>
2013-03-21 02:56:19 -0700

    add bunch of helper fxns

--- basparser.py
+++ basparser.py
@@ -10,7 +10,65 @@ import sdh_demo as sdh
 domain = [getattr(sdh, i) for i in sdh.__dict__ if isinstance(getattr(sdh,i), Relational)]
 
 def set_union(set1, set2):
-    return set(set1).issubset(set(set2))
+  return set(set1).issubset(set(set2))
+
+def isspatial(nodes):
+    """
+    returns True if [nodes] contains spatial objects
+    """
+    nodes = [nodes] if not isinstance(nodes,list) else nodes
+    return all(map(lambda x: not isinstance(x, (Relational, Node)),nodes))
+
+
+def links_to_nodes(links):
+    def _links_to_nodes( links):
+        for r in domain:
+            for node in r.nodes():
+                if str(node.uid) in [link.uuid for link in links]:
+                    yield node
+    return Node.NodeList(_links_to_nodes(links))
+
+def get_areas(gisobj):
+    """
+    returns a list of the areas for the given gisobj
+    """
+    areas = [gisobj] if not isinstance(gisobj,list) else gisobj
+    flattened = []
+    for place in areas:
+        if isinstance(place, gis.Building):
+            for floor in place.floors.all():
+                flattened.extend(list(floor.areas.all()))
+        elif isinstance(place, gis.Floor):
+            flattened.extend(list(place.areas.all()))
+        else:
+            flattened.append(place)
+    return flattened
+
+def areas_to_nodes(areas):
+    """
+    For every item in [areas], we resolve it down to all areas inside it and
+    then return the flat list of all nodes in those areas
+    """
+    areas = get_areas(areas)
+    return links_to_nodes(list(itertools.chain.from_iterable(x.nodes.all() for x in areas)))
+
+def nodes_to_areas(nodes):
+    nodes = [nodes] if not isinstance(nodes,list) else nodes
+    flattened = []
+    for node in nodes:
+      extension = node.areas.all()
+      if extension:
+        flattened.extend(extension)
+    return list(set(flattened))
+
+def resolve_spatial_nodes(querynodes, setnodes):
+    query_is_spatial = isspatial(querynodes)
+    set_is_spatial = isspatial(setnodes)
+    if query_is_spatial and not set_is_spatial:
+      setnodes = nodes_to_areas(setnodes)
+    elif not query_is_spatial and set_is_spatial:
+      setnodes = areas_to_nodes(setnodes)
+    return querynodes, setnodes
 
 class BasParser(object):
     basvars = dict()
