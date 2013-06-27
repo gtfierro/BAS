fe60ab3b1a147d00a1e414328bc736f2ee18b8e9
Gabe Fierro <gtfierro225@gmail.com>
2013-06-27 15:44:45 -0700

    add packages

--- manifests/default.pp
+++ manifests/default.pp
@@ -7,7 +7,8 @@ Package { ensure => present }
 $aptpackages = ['git','vim','tmux','postgresql','pgadmin3','gdal-bin',
                 'libspatialite3','libspatialite-dev','spatialite-bin',
                 'libgeos-dev','geoip-bin','libgeoip-dev', 'libxml2-dev',
-                'libxslt-dev','redis-server','python-pip','python-dev', 'curl']
+                'libxslt-dev','redis-server','python-pip','python-dev', 'curl',
+                'libcurl4-openssl-dev']
 
 package { $aptpackages:
     require => Exec['apt-get update'],
@@ -16,7 +17,7 @@ package { $aptpackages:
 $pippackages = ['django','django-olwidget','django-piston','fuzzywuzzy',
                 'ipython','networkx','PIL', 'ply','zope.event','smap','numpy',
                 'zope.interface','zope.schema', 'lxml', 'redis','requests',
-                'BeautifulSoup','pycurl']
+                'BeautifulSoup','PyCURL']
 
 package { $pippackages:
     require => Package[$aptpackages],
