d9e6505b37c8ba2046c2ac5fcec4ba73af56afb7
Gabe Fierro <gtfierro225@gmail.com>
2013-06-27 12:01:51 -0700

    include more packages

--- manifests/default.pp
+++ manifests/default.pp
@@ -7,7 +7,7 @@ Package { ensure => present }
 $aptpackages = ['git','vim','tmux','postgresql','pgadmin3','gdal-bin',
                 'libspatialite3','libspatialite-dev','spatialite-bin',
                 'libgeos-dev','geoip-bin','libgeoip-dev', 'libxml2-dev',
-                'libxslt-dev','redis-server','python-pip','python-dev']
+                'libxslt-dev','redis-server','python-pip','python-dev', 'curl']
 
 package { $aptpackages:
     require => Exec['apt-get update'],
@@ -15,7 +15,7 @@ package { $aptpackages:
 
 $pippackages = ['django','django-olwidget','django-piston','fuzzywuzzy',
                 'ipython','networkx','PIL', 'ply','zope.event',
-                'zope.interface','zope.schema', 'lxml']
+                'zope.interface','zope.schema', 'lxml', 'redis']
 
 package { $pippackages:
     require => Package[$aptpackages],
