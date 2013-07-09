391401a150adc78007688f46cb8aa4302d5a3335
Gabe Fierro <gtfierro225@gmail.com>
2013-07-09 12:10:17 -0700

    add geo admin file for geo models

---
+++ frontend/geo/admin.py
@@ -0,0 +1,17 @@
+from geo.models import Building, Floor, Area, Node
+from django.contrib.gis import admin
+from olwidget.admin import GeoModelAdmin
+
+#class OSMAdmin(admin.OSMGeoAdmin):
+#   zoom_level = 8
+#   default_lat = 37.871775
+#   default_lon = -122.274603
+
+class GoogleMapsAdmin(GeoModelAdmin):
+    options = {
+      'layers': ['google.streets'],
+      'default_lat': 37.871775,
+      'default_lon': -122.274603,
+      }
+
+admin.site.register([Building, Floor, Area, Node], GoogleMapsAdmin)
