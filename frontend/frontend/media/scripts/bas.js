69dc56e04d20bf7bd22b7a90f41880e14cbc69f2
Gabe Fierro <gtfierro225@gmail.com>
2013-07-09 11:31:19 -0700

    fix coordinates and zoomlevel

--- frontend/frontend/media/scripts/bas.js
+++ frontend/frontend/media/scripts/bas.js
@@ -1,5 +1,9 @@
 jQuery(function($) {
-  var yourStartLatLng = new google.maps.LatLng(37.87, -122.2705);
-  $('#map_canvas').gmap({'center': yourStartLatLng});
+  var yourStartLatLng = new google.maps.LatLng(37.870218, -122.259481);
+  $('#map_canvas').gmap({'center': yourStartLatLng, 'zoom': 16});
+
+  $('#map_canvas').on('click', function(e) {
+    console.log(e);
+  });
 });
 
