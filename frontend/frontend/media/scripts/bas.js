6134a84c04a7fbcbf322679d64701c3b3e69a0c0
Gabe Fierro <gtfierro225@gmail.com>
2013-07-09 17:53:48 +0000

    add jquery.ui.map js library

--- frontend/frontend/media/scripts/bas.js
+++ frontend/frontend/media/scripts/bas.js
@@ -1,10 +1,16 @@
 jQuery(function($) {
+  function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
   console.log('hi');
-  var map = Kartograph.map('#map', 600, 400);
-  map.loadMap('/static/map.svg', function(){;
-    console.log('map loaded?');
-  });
 
 });
 
