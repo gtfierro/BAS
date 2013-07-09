42b93d6818e9a758f4cfa55d3e36af3895b9511e
Gabe Fierro <gtfierro225@gmail.com>
2013-07-09 12:10:26 -0700

    include olwidget

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'olwidget',
     'geo',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
