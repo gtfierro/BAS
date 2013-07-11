dab6621b714dfbe58b1af14b31290b3cd3bbe54c
Gabe Fierro <gtfierro225@gmail.com>
2013-07-11 10:58:59 -0700

    remove unneeded postgres stuff

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,21 +1,5 @@
-exec { 'initial update':
-    command => '/usr/bin/apt-get update'
-}
-
-Package { 'python-software-properties':
-    ensure => present,
-    require => Exec['initial update'],
-}
-
-exec { 'add gis repo':
-    command => ['/usr/bin/apt-add-repository ppa:ubuntugis/ppa',
-                '/usr/bin/apt-add-repository ppa:kakrueger/openstreetmap']
-    require => Package['python-software-properties'],
-}
-
 exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
-  require => Exec['add gis repo'],
+  command => '/usr/bin/apt-get update',
 }
 
 Package { ensure => present }
@@ -24,8 +8,7 @@ $aptpackages = ['git','vim','tmux','postgresql','pgadmin3','gdal-bin',
                 'libspatialite3','libspatialite-dev','spatialite-bin',
                 'libgeos-dev','geoip-bin','libgeoip-dev', 'libxml2-dev',
                 'libxslt-dev','redis-server','python-pip','python-dev', 'curl',
-                'libcurl4-openssl-dev', 'postgresql-9.1-postgis', 'osm2pgsql',
-                'python-shapely', 'python-gdal', 'python-pypro', 'postgresql-server-dev-9.1']
+                'libcurl4-openssl-dev']
 
 package { $aptpackages:
     require => Exec['apt-get update'],
@@ -34,8 +17,7 @@ package { $aptpackages:
 $pippackages = ['django','django-olwidget','django-piston','fuzzywuzzy',
                 'ipython','networkx','PIL', 'ply','zope.event','smap','numpy',
                 'zope.interface','zope.schema', 'lxml', 'redis','requests',
-                'BeautifulSoup','PyCURL','GDAL','PyYAML','pykml','ordereddict',
-                'pyproj','tinycss','psycopg2']
+                'BeautifulSoup','PyYAML']
 
 package { $pippackages:
     require => Package[$aptpackages],
