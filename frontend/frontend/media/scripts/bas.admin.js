72f0f19fcb1b28872f22beeaf6bf145acb823331
vagrant <vagrant@precise64.(none)>
2013-07-28 01:16:06 +0000

    draw polygons on canvas

--- frontend/frontend/media/scripts/bas.admin.js
+++ frontend/frontend/media/scripts/bas.admin.js
@@ -2,6 +2,8 @@
 var $ = django.jQuery;
 var jQuery = django.jQuery;
 (function($) {
+  var floorplanid = '';
+  var canvascontext = '';
   $('#id_floor').live('change', function() {
     // get floor from dropdown
     var id_floor = $('#id_floor').val();
@@ -9,25 +11,48 @@ var jQuery = django.jQuery;
     // hide the other floors
     var floorobj
     $('.floorplan').each(function() {
+      floorplanid = 'floorplan_'+id_floor;
       console.log(this.id);
       console.log(id_floor);
-      if (this.id != 'floorplan_'+id_floor) {
+      if (this.id != floorplanid) {
         $(this).hide();
       } else {
         $(this).show();
+        canvascontext = this.getContext('2d');
         floorobj = this;
       }
     });
     
-    console.log($(floorobj));
-    
     // on clicking the image, get image coordinates
+    xcoords = new Array();;
+    ycoords = new Array();;
     $(floorobj).click( function(e) {
+        console.log('canvas?');
+        console.log($(this)[0]);
+        var ctx = $(this)[0].getContext('2d');
         var offset = $(this).offset();
-        console.log(e.clientX - offset.left);
-        console.log(e.clientY - offset.top);
+        console.log(offset);
+        var xcoord = e.pageX - offset.left;
+        var ycoord = e.pageY - offset.top;
+        console.log(xcoord);
+        console.log(ycoord);
+        ctx.rect(xcoord, ycoord, 8, 8);
+        ctx.fillStyle = 'red';
+        ctx.fill()
+        xcoords.push( xcoord );
+        ycoords.push( ycoord );
+        if (xcoords.length >= 2) {
+            ctx.lineWidth = 5;
+            ctx.beginPath();
+            ctx.moveTo(xcoords[xcoords.length-2], ycoords[ycoords.length-2]);
+            ctx.lineTo(xcoords[xcoords.length-1], ycoords[ycoords.length-1]);
+            ctx.strokeStyle = 'red';
+            ctx.closePath();
+            ctx.stroke();
+        }
         return true;
     });
     return false;
   });
+
 }(django.jQuery));
